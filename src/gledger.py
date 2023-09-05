ffaf880e744e838e269613e93a2e25d8d9610c42
Stuart Wayland <stuartwayland@ucsb.edu>
2023-09-05 11:01:41 +0100

    moving util functions + adding to git ignore

--- src/gledger.py
+++ src/gledger.py
@@ -12,50 +12,40 @@ class GLedger:
         receiving xlxs (or csv) General Ledger files. Only with general housekeeping, this class 
         supports some basic statiscal analysis and functions for generating projection lenses. 
     """
-      
-    def read_data(self, path:str):
-        """
-        Reads in an xlxs (or csv) file or a directory containing xlxs (or csv) files and returns a single DataFrame.
-        
-        Parameters
-        ----------
-        path: str 
-            Path to a raw general ledger file or directory 
-            
-        Returns
-        -------
-            A single dataframe sorted by company code and year
-        """
-        complete_df = pd.DataFrame
-        
-        if os.path.isdir(path):
-            file_names = [path+file for file in os.listdir(path) if (file.endswith('.csv') or file.endswith('.xlsx'))]
-            
-            assert len(file_names) > 0, "There do not seem to be any data files in your directory"
-            
-            df_list = [] 
-            for file in file_names:
-                if file.endswith(".xlsx"):
-                    xls = pd.ExcelFile(file)
-                    current_df = xls.parse('Sheet1', header=1)
-                else: 
-                    current_df = pd.read_csv(file)
-                df_list.append(current_df)
-            complete_df = pd.concat(df_list)
-            
-        
-        elif os.path.isfile(path):
-            if path.endswith('.xlsx'):
-                xls = pd.ExcelFile(path)
-                complete_df = xls.parse('Sheet1', header=1)
-            elif path.endswith('.csv'):
-                complete_df = pd.read_csv(path)
-        
-        
-        complete_df['$'] = complete_df['$'].apply(utils.convert_to_float)
-        complete_df.replace('#', 'n/a', inplace=True)
-        return complete_df.sort_values(by=["Company Code", "Fiscal Year"])
     
+    def __init__(self, t_lines=None, t_agg=None):
+        """
+        Intitializes GLedger class from a variety of build stages, supporting 
+        xlsx concatenation and conversion as well as line aggregation in flight for 
+        preparation towards pipelined capabilites with `krv_mapper`. 
+
+        t_lines: None, pd.DataFrame, str 
+            Passing None (ignores data) allows for use of read_data and agg_lines functionality for developement 
+            and testing when initlizing as None 
+
+            May pass an already compiled dataframe containing at the minimum columns 'Company Code', 'JE Doc #' 
+            'Fiscal Year', 'G/L Account' and '$'. 
+
+            May pass a path to a pickle file containing such a dataframe as described above. 
+
+            May pass a path to an xlsx file or folder, which will be compiled into a dataframe 
+            using the read_data function. 
+        
+        t_agg: None, pd.DataFrame, str 
+
+            Will be treated as `None` if a t_lines argument is not passed. 
+
+            If left `None`, t_agg will be computed from t_lines. 
+
+
+
+        """
+        if t_lines is not None: 
+            self._t_lines = pd.read_pickle()
+            self._t_agg = self.agg_lines(self._t_lines) 
+        self.flow_directions = None 
+    
+
 
     def clean_dtypes(self, df:pd.DataFrame):
         """
@@ -77,57 +67,22 @@ class GLedger:
         # STUB! 
         return df
 
-
-    def agg_lines(self, df:pd.DataFrame, single_cols=['Transaction Code', 'JE Doc Type', 'JE Created By'], agg_cols=['G/L Account']):
-        """
-        Aggregates Transaction lines by joining on Company Code + JE DOC # + Fiscal Year, 
-        recording agg_cols columns as sets and $ amount as net change over the transaction.  
-
-        Note: Will perform a pd.join with all columns in cols, so care must be taken to only include cols that are homogeneous across transaction lines.
-
-        Parameters:
-        -----------
-
-        df: pd.DataFrame 
-            A pandas dataframe representation of a General Ledger 
-        
-        single_cols: list 
-            A list of columns to be included in the complete transaction data set that are homogenous 
-            across transaction lines.
-
-
-        agg_cols: list 
-            A list of columns to be recorded as a set across transacation lines 
-        """
-        # Subset DataFrame
-        sub_df = df.copy()[single_cols + agg_cols + ['Company Code', 'JE Doc #', 'Fiscal Year', '$']]
-
-        # Convert Non-Set datatypes to strings
-        sub_df[single_cols] = sub_df[single_cols].astype(str)
-        
-        # Create Transaction ID column 
-        sub_df['ID'] = df['Company Code'].astype(str) + df['JE Doc #'].astype(str) + df['Fiscal Year'].astype(str)
-        
-
-        # Track total amount across transaction 
-        sub_df['$'] = sub_df['$'].apply(utils.abs_amount)
-        
-        single_agg = sub_df.drop_duplicates(subset=['ID'])[['ID'] + single_cols]
-
-        set_agg = sub_df.groupby('ID')[agg_cols].agg(set).reset_index()
-        amount_agg = sub_df.groupby('ID')['$'].sum().reset_index() 
-
-        agg = pd.merge(single_agg, set_agg, on='ID')
-        agg = pd.merge(agg, amount_agg, on='ID')
-        
-        return  agg
     
+    def bar(self, df:pd.DataFrame, col='G/L Account', threshold=0): 
+        """
+        Plots a bar graph of a given columns frequencies. 
 
-
-    def bar(self, df:pd.DataFrame, col='G/L Account', threshold=100): 
-        
-        #TODO: 
-        # Only apply tuple to set types. Currently not working as is. 
+        Parameters 
+        ----------
+            df: pd.DataFrame 
+                A dataframe of transaction (or transaction line) information 
+            col: 
+                The column to perform the frequency analysis and plotting of 
+            
+            threshold: int 
+                A cut-off point under which all occurences with smaller frequencies are 
+                removed from the plot.  
+        """
         
         if df[col].dtype == set:
             counts = df[col].apply(tuple).value_counts()
@@ -147,12 +102,49 @@ class GLedger:
         plt.show()
 
     
+
+    def fit(self, t_lines:pd.DataFrame, t_agg:pd.DataFrame): 
+        """
+        Computes pairwise interactions of G/L Accounts to determine common account flow 
+        directions. Creates a dictionary with keys being tuples of G/L Accounts and values 
+        being a length 4 array. 
+                (gl1,gl2)  ->   (++, +-, -+, --)
+
+                ++: number of transactions containing gl1 and gl2 where both gl1 and gl2 
+                recieve 
+                +-: " " where gl1 recieves money and gl2 deposits money 
+                -+: " " where gl1 deposits and gl2 receives 
+                -- " " where both gl1 and gl2 deposit 
+
+        Parameters 
+        ----------
+        t_lines: pd.DataFrame
+            A dataframe of transaction line information. Must have the columns 
+        'G/L Account' and '$'. 
+
+        t_agg: pd.DataFrame 
+            A dataframe of transactions containing a column 'G/L Account' that holds 
+            sets of G/L Accounts 
+        """
+        unique_accounts = sorted(t_lines['G/L Account'].unique()) 
+        account_pairs = []
+        for i in range(0,len(unique_accounts)):
+            for j in range(i+1, len(unique_accounts)): 
+                account_pairs.append(np.pair(unique_accounts[i], unique_accounts[j]))
+        
+        self.flow_directions = dict(account_pairs)
+
+        # for key_pair in self.flow_directions.keys(): 
     
-    def fit(): 
+
+
+    
+    def fit_transform(): 
         # STUB! 
         return 
     
-    def fit_transform(): 
+
+    def scatter():
         # STUB! 
         return 
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,99 @@
 
+import os 
+import pandas as pd
+import numpy as np
+
+def read_data(path:str):
+        """
+        Reads in an xlxs (or csv) file or a directory containing xlxs (or csv) files and returns a single DataFrame.
+        
+        Parameters
+        ----------
+        path: str 
+            Path to a raw general ledger file or directory 
+            
+        Returns
+        -------
+            A single dataframe sorted by company code and year
+        """
+        complete_df = pd.DataFrame
+        
+        if os.path.isdir(path):
+            file_names = [path+file for file in os.listdir(path) if (file.endswith('.csv') or file.endswith('.xlsx'))]
+            
+            assert len(file_names) > 0, "There do not seem to be any data files in your directory"
+            
+            df_list = [] 
+            for file in file_names:
+                if file.endswith(".xlsx"):
+                    xls = pd.ExcelFile(file)
+                    current_df = xls.parse('Sheet1', header=1)
+                else: 
+                    current_df = pd.read_csv(file)
+                df_list.append(current_df)
+            complete_df = pd.concat(df_list)
+            
+        
+        elif os.path.isfile(path):
+            if path.endswith('.xlsx'):
+                xls = pd.ExcelFile(path)
+                complete_df = xls.parse('Sheet1', header=1)
+            elif path.endswith('.csv'):
+                complete_df = pd.read_csv(path)
+        
+        
+        complete_df['$'] = complete_df['$'].apply(convert_to_float)
+        complete_df.replace('#', 'n/a', inplace=True)
+        return complete_df.sort_values(by=["Company Code", "Fiscal Year"])
+
+
+
+def agg_lines(self, df:pd.DataFrame, single_cols=['Transaction Code', 'JE Doc Type', 'JE Created By'], agg_cols=['G/L Account']):
+    """
+    Aggregates Transaction lines by joining on Company Code + JE DOC # + Fiscal Year, 
+    recording agg_cols columns as sets and $ amount as net change over the transaction.  
+
+    Note: Will perform a pd.join with all columns in cols, so care must be taken to only include cols that are homogeneous across transaction lines.
+
+    Parameters:
+    -----------
+
+    df: pd.DataFrame 
+        A pandas dataframe representation of a General Ledger 
+    
+    single_cols: list 
+        A list of columns to be included in the complete transaction data set that are homogenous 
+        across transaction lines.
+
+
+    agg_cols: list 
+        A list of columns to be recorded as a set across transacation lines 
+    """
+    # Subset DataFrame
+    sub_df = df.copy()[single_cols + agg_cols + ['Company Code', 'JE Doc #', 'Fiscal Year', '$']]
+
+    # Convert Non-Set datatypes to strings
+    sub_df[single_cols] = sub_df[single_cols].astype(str)
+    
+    # Create Transaction ID column 
+    sub_df['ID'] = df['Company Code'].astype(str) + df['JE Doc #'].astype(str) + df['Fiscal Year'].astype(str)
+    
+
+    # Track total amount across transaction 
+    sub_df['$'] = sub_df['$'].apply(abs_amount)
+    
+    single_agg = sub_df.drop_duplicates(subset=['ID'])[['ID'] + single_cols]
+
+    set_agg = sub_df.groupby('ID')[agg_cols].agg(set).reset_index()
+    amount_agg = sub_df.groupby('ID')['$'].sum().reset_index() 
+
+    agg = pd.merge(single_agg, set_agg, on='ID')
+    agg = pd.merge(agg, amount_agg, on='ID')
+
+    # TODO: Sort by Fiscal Year, then JE Doc #, then Company Code in 'ID' 
+    
+    return  agg
+
 
 def convert_to_float(value):
     try:
